7ef307894d47e63ad6bfab3dd2d89d398fcf74d5
acomarcho <acoxstpd@gmail.com>
2023-07-26 19:00:29 +0700

    Feat: Initialize website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Playfair_Display, Open_Sans } from "next/font/google";
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${playfairDisplay.variable} ${openSans.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
